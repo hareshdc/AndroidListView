85cbfec5ff16eb44f4264917473582e0b2a81e6a
DESKTOP-6ICSE8H\cddat <hareshdc@gmail.com>
2018-05-12 15:33:20 -0700

    User List screen, where all the users will be added to the list and shown

---
+++ ListViewExample/ListViewExample/ViewUsersActivity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using ListViewExample.Helpers;
+
+namespace ListViewExample
+{
+    [Activity(Label = "ViewUsersActivity")]
+    public class ViewUsersActivity : ListActivity
+    {
+        List<Users> usersList = new List<Users>();
+        List<User> contactList = Settings.Users;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //add the list to the list adapter
+            ListAdapter = new ArrayAdapter<User>(this, Android.Resource.Layout.SimpleListItem1, contactList);
+        }
+    }
+}
